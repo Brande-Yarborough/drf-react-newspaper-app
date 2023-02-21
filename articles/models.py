da464b21b85c4c97104abcae06d9fa4690799dcf
BrandeYarborough <brandeyarborough@gmail.com>
2023-02-21 12:16:12 -0500

    installs react bootstrap

--- articles/models.py
+++ articles/models.py
@@ -7,6 +7,10 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=255)
 
+    class Meta:
+        # changes model name to plural so it is spelled correctly in admin
+        verbose_name_plural = "categories"
+
     def __str__(self):
         return self.title
 
