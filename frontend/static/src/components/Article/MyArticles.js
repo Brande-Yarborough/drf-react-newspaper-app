7d95585f8c69347e28e805fbd3ffd4da46324b82
BrandeYarborough <brandeyarborough@gmail.com>
2023-02-28 22:47:09 -0500

    attempt at my articles edit feature

--- frontend/static/src/components/Article/MyArticles.js
+++ frontend/static/src/components/Article/MyArticles.js
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+
 function MyArticles() {
+  const [myArticles, setMyArticles] = useState(null);
+
+  useEffect(() => {
+    const getMyArticles = async () => {
+      const response = await fetch(`/api_v1/articles/`);
+      if (!response.ok) {
+        throw new Error("Network response was not OK");
+      }
+
+      const data = await response.json();
+      setMyArticles(data);
+    };
+    getMyArticles();
+  }, []);
+
   return <div>My articles</div>;
 }
 
